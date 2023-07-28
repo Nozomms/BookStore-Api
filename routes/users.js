0511cc37f844019f841b897d9a0ed288d1fad41f
Nozomms <nozomms@gmail.com>
2023-07-28 16:59:49 +0300

    add authContraller

--- routes/users.js
+++ routes/users.js
@@ -1,86 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const asyncHandler = require("express-async-handler");
-const { User, validateUpdateUser } = require("../models/User")
-const {  verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("../middlewares/verifyToken")
-
-/**
- * @desc Update User
- * @route /api/users/:id
- * @method PUT
- * @access private
- */
-
-router.put("/:id", verifyTokenAndAuthorization, asyncHandler(async (req, res) => {
-  const { error } = validateUpdateUser(req.body);
-  if (error) {
-    return res.status(400).json({ message: error.details[0].message })
-  }
-
-  if (req.body.password) {
-    const salt = await bcrypt.genSalt(10);
-    req.body.password = await bcrypt.hash(req.body.password, salt)
-  }
-
-  const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-    $set: {
-      email: req.body.email,
-      password: req.body.password,
-      username: req.body.username
-    }
-  }, { new: true }).select("-password");
-  res.status(200).json(updatedUser);
-}))
+const { verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("../middlewares/verifyToken")
+const { updateUser, getAllUser, getUserById, deletUserById } = require("../controllers/userController")
 
 
-/**
- * @desc GET ALL User
- * @route /api/users
- * @method GET
- * @access private (only admin)
- */
+router.put("/:id", verifyTokenAndAuthorization, updateUser);
 
-router.get("/", verifyTokenAndAdmin, asyncHandler(async (req, res) => {
-  const users = await User.find().select("-password");
-  res.status(200).json(users);
-}))
+router.get("/", verifyTokenAndAdmin, getAllUser)
 
+router.get("/:id", verifyTokenAndAuthorization, getUserById)
 
-/**
- * @desc GET User By Id
- * @route /api/users/:id
- * @method GET
- * @access private (only admin & user himself)
- */
-
-router.get("/:id", verifyTokenAndAuthorization, asyncHandler(async (req, res) => {
-  const user = await User.findById().select("-password");
-  res.status(200).json(user);
-  if(user){
-    res.status(200).json(user);
-  } else {
-    res.status(404).json({message: "user not found" });
-  }
-}))
-
-
-/**
- * @desc Delet User By Id
- * @route /api/users/:id
- * @method DELET
- * @access private (only admin & user himself)
- */
-
-router.delete("/:id", verifyTokenAndAuthorization, asyncHandler(async (req, res) => {
-  const user = await User.findById().select("-password");
-  res.status(200).json(user);
-  if(user){
-    await User.findByIdAndDelet(req.params.id)
-    res.status(200).json({message: "user has been deleted"});
-  } else {
-    res.status(404).json({message: "the user you are trying to delet not found" });
-  }
-}))
+router.delete("/:id", verifyTokenAndAuthorization, deletUserById)
 
 module.exports = router;
